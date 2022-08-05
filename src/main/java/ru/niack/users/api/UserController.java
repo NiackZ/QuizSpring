9c23f6630ef6eb120dd89a7c8e6160a30564ae4c
NiackZ <niack94@gmail.com>
2022-08-05 23:35:50 +0500

    Реализация методов в UserController

--- src/main/java/ru/niack/users/api/UserController.java
+++ src/main/java/ru/niack/users/api/UserController.java
@@ -1,10 +1,43 @@
 package ru.niack.users.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+import ru.niack.users.entity.User;
+import ru.niack.users.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @GetMapping()
+  public List<User> getAll(){
+    return userService.findAll();
+  }
+
+  @GetMapping("{id}")
+  public User getById(@PathVariable Long id){
+    return userService.findById(id);
+  }
+
+  @PostMapping
+  public Long create(User userData){
+    return userService.add(userData);
+  }
+
+  @PutMapping("{id}")
+  public Long create(@PathVariable @NonNull Long id, User userData){
+    return userService.update(id, userData);
+  }
+
+  @DeleteMapping("{id}")
+  public Long delete(@PathVariable @NonNull Long id, User userData){
+    return userService.delete(id, userData);
+  }
 
 }

0448ddd44178bea177ffa19cad1d4e2748d8b27c
NiackZ <niack94@gmail.com>
2022-08-05 22:32:49 +0500

    Базовые методы в UserService

--- src/main/java/ru/niack/users/service/UserService.java
+++ src/main/java/ru/niack/users/service/UserService.java
@@ -1,4 +1,40 @@
 package ru.niack.users.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.niack.users.entity.User;
+import ru.niack.users.repository.IUserRepository;
+
+import java.util.List;
+
+@Service
 public class UserService {
+  @Autowired
+  private IUserRepository userRepository;
+
+  public User findById(Long id){
+    return this.userRepository.findById(id).orElseThrow(RuntimeException::new);
+  }
+
+  public List<User> findAll(){
+    return this.userRepository.findAll();
+  }
+
+  public Long add(User userData) {
+    User user = userData.getId() == null ? new User() : userData;
+    this.userRepository.save(user);
+    return 1L;
+  }
+
+  public Long update(Long id, User userData){
+    userData.setId(id);
+    return add(userData);
+  }
+
+  public Long delete(Long id, User userData){
+    userData.setId(id);
+    userData.setDeleted(true);
+    return add(userData);
+  }
+
 }
